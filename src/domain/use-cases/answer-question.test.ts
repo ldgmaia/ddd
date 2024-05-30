6eeba6af6c06ebee1d4036b1b4e0aecfe111bc68
Diego Maia <ldgmaia@gmail.com>
2024-05-30 09:24:00 -0400

    External dependencies implementation

--- src/domain/use-cases/answer-question.test.ts
+++ src/domain/use-cases/answer-question.test.ts
@@ -1,10 +1,18 @@
 import { describe, expect, it } from 'vitest'
+import type { Answer } from '../entities/answer'
+import type { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return
+  },
+}
+
 describe('create an answer', () => {
-  it('should be able to answer a question', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
-    const answer = answerQuestion.execute({
+  it('should be able to answer a question', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+    const answer = await answerQuestion.execute({
       content: 'New answer',
       questionId: '1',
       instructorId: '1',
@@ -12,13 +20,13 @@ describe('create an answer', () => {
     expect(answer.content).toEqual('New answer')
   })
 
-  it('should be able to answer a question3', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
-    const answer = answerQuestion.execute({
-      content: 'New answer',
-      questionId: '1',
-      instructorId: '1',
-    })
-    expect(answer.content).toEqual('New answer')
-  })
+  // it('should be able to answer a question3', () => {
+  //   const answerQuestion = new AnswerQuestionUseCase()
+  //   const answer = answerQuestion.execute({
+  //     content: 'New answer',
+  //     questionId: '1',
+  //     instructorId: '1',
+  //   })
+  //   expect(answer.content).toEqual('New answer')
+  // })
 })
