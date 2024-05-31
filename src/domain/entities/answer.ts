57edab456403270c862d2f51d315e21233f36bf5
Diego Maia <ldgmaia@gmail.com>
2024-05-31 10:09:17 -0400

    Feat: Abstracting entity creation

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,12 +1,30 @@
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityId } from '../../core/entities/unique-entity-id'
+import type { Optional } from '../../core/types/optional'
 
 interface AnswerProps {
+  authorId: UniqueEntityId
+  questionId: UniqueEntityId
   content: string
-  authorId: string
-  questionId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content
+  }
+
+  static create(
+    props: Optional<AnswerProps, 'createdAt'>,
+    id?: UniqueEntityId
+  ) {
+    const answer = new Answer(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    )
+    return answer
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,29 @@
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityId } from '../../core/entities/unique-entity-id'
+import type { Optional } from '../../core/types/optional'
 import type { Slug } from './value-objects/slug'
 
 interface QuestionProps {
+  authorId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
-  slug: Slug
   content: string
-  authorId: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(
+    props: Optional<QuestionProps, 'createdAt'>,
+    id?: UniqueEntityId
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id
+    )
+    return question
+  }
+}
